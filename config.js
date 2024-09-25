60edf010cac7a8c35500632da176c7b95289572d
KING11223344vr <K1N611223344@outlook.com>
2024-09-25 08:32:00 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,26 +1,10 @@
-self.__dynamic$config = {
-	prefix: "/a/q/",
-	encoding: "xor",
-	mode: "production",
-	logLevel: 0,
-	bare: {
-		version: 2,
-		path: "/ov/",
-	},
-	tab: {
-		title: null,
-		icon: null,
-		ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.3",
-	},
-	assets: {
-		prefix: "/assets/-dy/",
-		files: {
-			handler: "handler.js?v=6-17-2024",
-			client: "client.js?v=12",
-			worker: "worker.js?v=12",
-			config: "config.js?v=6-17-2024",
-			inject: "",
-		},
-	},
-	block: [],
-};
+self.__uv$config = {
+  prefix: "/a/",
+  bare: "/ov/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/assets/-uv/handler.js?v=6-17-2024",
+  bundle: "/assets/-uv/bundle.js?v=6-17-2024",
+  config: "/assets/-uv/config.js?v=6-17-2024",
+  sw: "/assets/-uv/sw.js?v=6-17-2024",
+}
